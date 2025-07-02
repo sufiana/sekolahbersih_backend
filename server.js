feca701a00105483b457408af364f644d5c6050e
sufiana <sufiana.az15@gmail.com>
2025-07-02 14:02:41 +0700

    perbaiki server.js port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./src/index");
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0"; // ← tambahkan ini
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 Server running on http://${HOST}:${PORT}`);
 });
